b6c11ef0afe74daa857ffe4bdff27b26f577080a
玲音 <zlainsama@163.com>
2013-04-30 16:03:10 +0800

    missing setup

--- src/main/java/lain/mods/inputfix/InputFix.java
+++ src/main/java/lain/mods/inputfix/InputFix.java
@@ -1,6 +1,9 @@
 package lain.mods.inputfix;
 
+import java.io.File;
 import lain.mods.laincraft.Plugin;
+import com.google.common.eventbus.Subscribe;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class InputFix extends Plugin
 {
@@ -9,6 +12,12 @@ public class InputFix extends Plugin
     public String getName()
     {
         return "InputFix";
+    }
+
+    @Subscribe
+    public void init(FMLPreInitializationEvent event)
+    {
+        InputFix_Config.setup(new File(event.getModConfigurationDirectory(), "InputFix.cfg"));
     }
 
 }
